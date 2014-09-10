3af82c307620cb93bc3d09ff6bc0c07f61a7e68d
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-09-09 22:25:57 -0400

    Make the homepage work

---
+++ spec/features/view_homepage_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature "View the homepage" do
+  scenario "user sees relevant information" do
+    visit root_path
+
+    expect(page).to have_title "Todos"
+    expect(page).to have_css '[data-role="description"]'
+  end
+end
